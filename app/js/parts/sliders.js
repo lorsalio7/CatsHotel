72ef22c58a629e80ffd87046f4daec8ce1edb83c
Lorsalio7 <lorsalio7@gmail.com>
2023-08-06 21:44:20 +0500

    Сверстана секция 'Отзывы'

--- app/js/parts/sliders.js
+++ app/js/parts/sliders.js
@@ -1,7 +1,23 @@
 document.addEventListener( 'DOMContentLoaded', function() {
   let roomsSlider = document.querySelector(".rooms-slider");
+  let reviewsSlider = document.querySelector(".reviews__slider");
   if(roomsSlider) {
     new Splide(roomsSlider, {
+      gap: 30,
+      mediaQuery: 'min',
+      breakpoints: {
+        200: {
+          arrows: false,
+        },
+        798: {
+          arrows: true,
+        },
+      }
+    }).mount();
+  }
+
+  if(reviewsSlider) {
+    new Splide(reviewsSlider, {
       gap: 30,
       mediaQuery: 'min',
       breakpoints: {
--- dist/js/script.js
+++ dist/js/script.js
@@ -27,8 +27,23 @@ if (burgerButton) {
 ;
 document.addEventListener('DOMContentLoaded', function () {
   var roomsSlider = document.querySelector(".rooms-slider");
+  var reviewsSlider = document.querySelector(".reviews__slider");
   if (roomsSlider) {
     new Splide(roomsSlider, {
+      gap: 30,
+      mediaQuery: 'min',
+      breakpoints: {
+        200: {
+          arrows: false
+        },
+        798: {
+          arrows: true
+        }
+      }
+    }).mount();
+  }
+  if (reviewsSlider) {
+    new Splide(reviewsSlider, {
       gap: 30,
       mediaQuery: 'min',
       breakpoints: {
